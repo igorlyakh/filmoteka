c351117fd6d9afddf453f59ed1e671572fb94508
Igor Liakh <igorlyah060@gmail.com>
2024-11-22 18:25:11 +0200

    cofig project

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module 'modern-normalize' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
