f0b2d6a3e23c82a80e40daba1b24161b23cc36ed
Igor Liakh <igorlyah060@gmail.com>
2024-11-22 20:56:30 +0200

    added base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<div>Home</div>}
+      />
+      <Route
+        path="login"
+        element={<div>Login</div>}
+      />
+      <Route
+        path="registration"
+        element={<div>Registration</div>}
+      />
+      <Route
+        path="*"
+        element={<div>Not found</div>}
+      />
+    </Routes>
+  );
 };
 
 export default App;
