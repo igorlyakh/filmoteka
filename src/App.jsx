8abaab84741208e5833fcf990bfce30526d56859
Igor Liakh <igorlyah060@gmail.com>
2024-11-26 19:33:22 +0200

    update project for js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import Layout from '@/Components/Layout';
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Layout />}
+      >
+        <Route
+          index
+          element={<div>Home Page</div>}
+        />
+        <Route
+          path="login"
+          element={<div>Login Page</div>}
+        />
+        <Route
+          path="registration"
+          element={<div>Registration Page</div>}
+        />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
