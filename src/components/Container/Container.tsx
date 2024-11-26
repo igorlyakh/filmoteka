2a7b382a829afde544e737431ad48d84aec31bf0
Igor Liakh <igorlyah060@gmail.com>
2024-11-26 19:11:05 +0200

    migrate to js

--- src/components/Container/Container.tsx
+++
@@ -1,8 +0,0 @@
-import { FC, PropsWithChildren } from 'react';
-import style from './Container.module.scss';
-
-const Container: FC<PropsWithChildren> = ({ children }) => {
-  return <div className={style.container}>{children}</div>;
-};
-
-export default Container;
