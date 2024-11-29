640fd175c6586e5d15b5f0c51ec228a5e7c7ac3e
Igor Liakh <igorlyah060@gmail.com>
2024-11-29 18:16:25 +0200

    added state in Layout

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,26 +1,35 @@
 import Container from '@/components/Container';
 import styled from './Layout.module.scss';
 
+import useAuthStore from '@/store/store';
 import { Link, Outlet } from 'react-router-dom';
 
 const Layout = () => {
+  const { token } = useAuthStore();
+
   return (
     <>
       <header className={styled.header}>
         <Link to="/">LOGO</Link>
         <menu className={styled.menu}>
-          <Link
-            className={styled.link}
-            to="login"
-          >
-            Войти
-          </Link>
-          <Link
-            className={styled.link}
-            to="registration"
-          >
-            Регистрация
-          </Link>
+          {token ? (
+            <button>Выйти</button>
+          ) : (
+            <>
+              <Link
+                className={styled.link}
+                to="login"
+              >
+                Войти
+              </Link>
+              <Link
+                className={styled.link}
+                to="registration"
+              >
+                Регистрация
+              </Link>
+            </>
+          )}
         </menu>
       </header>
       <main>
