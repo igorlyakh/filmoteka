f71a6fa437b4a1f546b185b0490f825b686b158b
Igor Liakh <igorlyah060@gmail.com>
2024-11-29 18:08:24 +0200

    update store

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,12 @@ import axiosInstance from '@/api/axios';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
+const setData = (token, name, email) => {
+  localStorage.setItem('token', JSON.stringify(token));
+  localStorage.setItem('name', JSON.stringify(name));
+  localStorage.setItem('email', JSON.stringify(email));
+};
+
 const useAuthStore = create(
   devtools(set => ({
     token: JSON.parse(localStorage.getItem('token')) || null,
@@ -14,9 +20,7 @@ const useAuthStore = create(
       try {
         const res = await axiosInstance.post('/auth/login', data);
         const { accessToken, name, email } = await res.data;
-        localStorage.setItem('token', JSON.stringify(accessToken));
-        localStorage.setItem('name', JSON.stringify(name));
-        localStorage.setItem('email', JSON.stringify(email));
+        setData(accessToken, name, email);
         set({ token: accessToken, name, email });
       } catch (error) {
         console.log(error.response?.data?.message);
@@ -30,9 +34,7 @@ const useAuthStore = create(
       try {
         const res = await axiosInstance.post('/auth/registration', data);
         const { accessToken, name, email } = res.data;
-        localStorage.setItem('token', JSON.stringify(accessToken));
-        localStorage.setItem('name', JSON.stringify(name));
-        localStorage.setItem('email', JSON.stringify(email));
+        setData(accessToken, name, email);
         set({ token: accessToken, name, email });
       } catch (error) {
         set({ error: error.response?.data?.message });
