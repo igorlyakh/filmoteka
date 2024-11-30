4db7c320d151a714e3ae49bff05deb0fe8863ad9
Igor Liakh <igorlyah060@gmail.com>
2024-11-30 20:06:27 +0200

    added react hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import '@/styles/global.scss';
 import 'modern-normalize';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster position="top-right" />
     </BrowserRouter>
   </StrictMode>
 );
