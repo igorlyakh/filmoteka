584e6be11da2546d41ce4fd62d620826c99b1796
Igor Liakh <igorlyah060@gmail.com>
2024-12-01 19:43:05 +0200

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import Layout from '@/components/Layout';
 import RegistrationPage from '@/pages/RegistrationPage';
 import RestrictedRoutes from '@/routes/RestrictedRoutes';
+import useAuthStore from '@/store/store';
 import { Route, Routes } from 'react-router-dom';
+import { PuffLoader } from 'react-spinners';
 import LoginPage from './pages/LoginPage';
 
 const App = () => {
+  const { isLoading } = useAuthStore();
+
+  if (isLoading) {
+    return (
+      <PuffLoader
+        color="#ffffff"
+        size={100}
+        cssOverride={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      />
+    );
+  }
+
   return (
     <Routes>
       <Route
