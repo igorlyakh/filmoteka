223cb184b373c16454603d0fcabfb7b75fcb32dd
Igor Liakh <igorlyah060@gmail.com>
2024-12-03 18:17:00 +0200

    added validation on form submit

--- src/Components/AuthForm/AuthForm.jsx
+++ src/Components/AuthForm/AuthForm.jsx
@@ -1,15 +1,28 @@
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import style from './AuthForm.module.scss';
 
 const AuthForm = ({ typeRegistration = false, handler }) => {
-  const { register, handleSubmit, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onError = errors => {
+    Object.values(errors).forEach(errors => {
+      toast.error(errors.message);
+    });
+  };
+
   return (
     <form
       className={style.form}
       onSubmit={handleSubmit(data => {
         handler(data);
         reset();
-      })}
+      }, onError)}
     >
       {typeRegistration && (
         <input
@@ -17,7 +30,9 @@ const AuthForm = ({ typeRegistration = false, handler }) => {
           placeholder="Введите Ваше имя"
           type="text"
           required
-          {...register('name')}
+          {...register('name', {
+            required: 'Имя обязательно для заполнения!',
+          })}
         />
       )}
       <input
@@ -25,14 +40,26 @@ const AuthForm = ({ typeRegistration = false, handler }) => {
         placeholder="Введите Ваш email"
         type="email"
         required
-        {...register('email')}
+        {...register('email', {
+          required: 'Email обязателен для заполнения!',
+          pattern: {
+            value: /^[a-zA-z0-9_.+-]+@[a-zA-z0-9-]+\.[a-zA-z0-9-.]+$/,
+            message: 'Введите корректный email!',
+          },
+        })}
       />
       <input
         className={style.input}
         placeholder="Введите Ваш пароль"
         type="password"
         required
-        {...register('password')}
+        {...register('password', {
+          required: 'Пароль обязателен для заполнения!',
+          minLength: {
+            value: 6,
+            message: 'Пароль должен быть не менее 6 символов!',
+          },
+        })}
       />
       <button
         className={style.button}
