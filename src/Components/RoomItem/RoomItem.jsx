d7f23b5db7675a5508919363aa7edcb0bfd2b3a3
Igor Liakh <igorlyah060@gmail.com>
2024-12-09 20:13:08 +0200

    added delete btn

--- src/Components/RoomItem/RoomItem.jsx
+++ src/Components/RoomItem/RoomItem.jsx
@@ -1,10 +1,14 @@
 import { IoIosArrowForward } from 'react-icons/io';
+import { TiDelete } from 'react-icons/ti';
 import { Link } from 'react-router-dom';
 import style from './RoomItem.module.scss';
 
 const RoomItem = ({ name, roomId }) => {
   return (
     <li className={style.item}>
+      <button className={style.deleteButton}>
+        <TiDelete />
+      </button>
       <p>{name}</p>
       <Link
         className={style.link}
