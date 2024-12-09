41b9b385d725e4d191cc447a3d11a0edf30a39f8
Igor Liakh <igorlyah060@gmail.com>
2024-12-09 20:28:27 +0200

    added norify about delete room

--- src/api/deleteRoom.js
+++ src/api/deleteRoom.js
@@ -1,8 +1,10 @@
+import toast from 'react-hot-toast';
 import axiosInstance from './axios';
 
 const deleteRoom = async roomId => {
   try {
     const res = await axiosInstance.delete('/room', { data: { roomId } });
+    toast.success('Комната успешно удалена!');
     return res.data;
   } catch (error) {
     return error.response?.data?.message;
