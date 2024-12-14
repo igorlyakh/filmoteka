32f88f596df00b6a74d9c85894c87af13be7767e
Igor Liakh <igorlyah060@gmail.com>
2024-12-14 17:01:27 +0200

    added logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,12 @@ import styled from './Header.module.scss';
 const Header = () => {
   return (
     <header className={styled.header}>
-      <Link to="/">LOGO</Link>
+      <Link
+        className={styled.logo}
+        to="/"
+      >
+        Filmoteka
+      </Link>
       <Menu />
     </header>
   );
