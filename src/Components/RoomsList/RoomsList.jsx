0c4a0705cb06e32a6a41d325452ba1805904fcf9
Igor Liakh <igorlyah060@gmail.com>
2024-12-26 16:23:11 +0200

    refactored RoomsList

--- src/Components/RoomsList/RoomsList.jsx
+++ src/Components/RoomsList/RoomsList.jsx
@@ -1,18 +1,11 @@
-import getRooms from '@/api/getRooms';
-import useAuthStore from '@/store/store';
-import { useEffect, useState } from 'react';
-import { io } from 'socket.io-client';
+import { useState } from 'react';
 import AddRoom from '../AddRoom';
 import EmptyHeader from '../EmptyHeader';
 import RoomItem from '../RoomItem';
 import style from './RoomsList.module.scss';
 
-const RoomsList = () => {
-  const [rooms, setRooms] = useState([]);
-
+const RoomsList = ({ rooms, setRooms }) => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const { token } = useAuthStore();
 
   const onDeleteRoom = roomId => {
     setRooms(prevRooms => {
@@ -24,43 +17,6 @@ const RoomsList = () => {
     setIsOpen(prev => !prev);
   };
 
-  useEffect(() => {
-    const socket = io('http://localhost:3001', {
-      withCredentials: true,
-      extraHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    socket.on('addToRoom', data => {
-      setRooms(prevRooms => {
-        if (Array.isArray(prevRooms)) {
-          return [...prevRooms, data];
-        } else {
-          return [data];
-        }
-      });
-    });
-
-    socket.on('kickFromRoom', data => {
-      setRooms(prevRooms => {
-        return prevRooms.filter(room => room.id !== data.id);
-      });
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, [token]);
-
-  useEffect(() => {
-    const getData = async () => {
-      const data = await getRooms();
-      setRooms(data);
-    };
-
-    getData();
-  }, []);
   return (
     <>
       <AddRoom
--- src/pages/RoomsPage/RoomsPage.jsx
+++ src/pages/RoomsPage/RoomsPage.jsx
@@ -1,7 +1,58 @@
+import getRooms from '@/api/getRooms';
 import RoomsList from '@/components/RoomsList';
+import useAuthStore from '@/store';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 
 const RoomsPage = () => {
-  return <RoomsList />;
+  const [rooms, setRooms] = useState([]);
+
+  const { token } = useAuthStore();
+
+  useEffect(() => {
+    const socket = io('http://localhost:3001', {
+      withCredentials: true,
+      extraHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    socket.on('addToRoom', data => {
+      setRooms(prevRooms => {
+        if (Array.isArray(prevRooms)) {
+          return [...prevRooms, data];
+        } else {
+          return [data];
+        }
+      });
+    });
+
+    socket.on('kickFromRoom', data => {
+      setRooms(prevRooms => {
+        return prevRooms.filter(room => room.id !== data.id);
+      });
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [token]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getRooms();
+      setRooms(data);
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <RoomsList
+      rooms={rooms}
+      setRooms={setRooms}
+    />
+  );
 };
 
 export default RoomsPage;
