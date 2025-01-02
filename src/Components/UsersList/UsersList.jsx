094453b6eaf356aea7b1358ed0a71c329337676c
Igor Liakh <igorlyah060@gmail.com>
2025-01-03 00:54:49 +0200

    added title for users list

--- src/Components/UsersList/UsersList.jsx
+++ src/Components/UsersList/UsersList.jsx
@@ -3,23 +3,26 @@ import style from './UsersList.module.scss';
 
 const UserList = ({ users }) => {
   return (
-    <ul className={style.list}>
-      {users.map(user => (
-        <li
-          className={style.listItem}
-          key={user.id}
-        >
-          <span>Имя: </span>
-          <span>{user.name}</span>
-          <button
-            type="button"
-            className={style.btn}
+    <>
+      <h2 className={style.title}>Пользователи в комнате:</h2>
+      <ul className={style.list}>
+        {users.map(user => (
+          <li
+            className={style.listItem}
+            key={user.id}
           >
-            <IoIosRemoveCircleOutline />
-          </button>
-        </li>
-      ))}
-    </ul>
+            <span>Имя: </span>
+            <span>{user.name}</span>
+            <button
+              type="button"
+              className={style.btn}
+            >
+              <IoIosRemoveCircleOutline />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
