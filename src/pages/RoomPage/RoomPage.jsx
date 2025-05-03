6e162a17431737448c3ed74162d3894d532d7bf3
Igor Liakh <igorlyah060@gmail.com>
2025-05-03 18:20:21 +0300

    added AddBtn on RoomPage

--- src/pages/RoomPage/RoomPage.jsx
+++ src/pages/RoomPage/RoomPage.jsx
@@ -1,18 +1,27 @@
+import AddBtn from '@/components/AddBtn';
 import BackLink from '@/components/BackLink';
 import MovieList from '@/components/MovieList';
 import UserList from '@/components/UsersList';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const RoomPage = () => {
   const location = useLocation();
   const backLink = useRef(location.state?.from ?? '/rooms');
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => {
+    setIsOpen(prev => !prev);
+  };
 
   return (
     <>
       <BackLink path={backLink.current} />
       <UserList />
       <MovieList />
+      <AddBtn
+        handler={toggleModal}
+        text="Добавить фильм"
+      />
     </>
   );
 };
