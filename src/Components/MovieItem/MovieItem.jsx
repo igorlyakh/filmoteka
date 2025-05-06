fc0d3bbe678d6913c28b31050e6171ff09f03555
Igor Liakh <igorlyah060@gmail.com>
2025-05-06 18:58:59 +0300

    added styles for movie item

--- src/Components/MovieItem/MovieItem.jsx
+++ src/Components/MovieItem/MovieItem.jsx
@@ -1,12 +1,21 @@
+import { IoIosRemoveCircleOutline } from 'react-icons/io';
+import styles from './MovieItem.module.scss';
+
 const MovieItem = ({ title, poster }) => {
   return (
-    <li>
-      <p>{title}</p>
+    <li className={styles.movieItem}>
+      <button
+        className={styles.btn}
+        type="button"
+      >
+        <IoIosRemoveCircleOutline />
+      </button>
       <img
         src={poster}
         alt={title}
         width={200}
       />
+      <p>{title}</p>
     </li>
   );
 };
